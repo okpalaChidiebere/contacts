1e56e2142fe45ab552e7b2b7e4b6bd1a99e5f9b2
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-12-13 01:17:19 -0600

    Composing with Components
    Composition is the ability to encapsulate a bunch of elements in a component and then easily reuse them and then also the ability to configure those components differently and independently via props() are two really important and fundamental property to understand composition of models in React.
    Using Components encourages us to build our app using composition instead of inheritance.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts //this way we can initilalze the data dynamically inside the JSX when with the data passed to the props of this component instead of hard coding the data in here
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+
+class App extends Component {
+  render() {	
+    //easily reusing all the elements is the reason why we encapsulate many elements inside a componets!
+    //we also have a clean interface so that we can configure each components nicely like we did below by just giving then different 'props'
+    return (
+      <div className="Apps">
+      <ContactList contacts={[{ name: 'Tyler '},{ name: 'Karen' },{ name: 'Richard '}]}/>
+      <ContactList contacts={[{ name: 'chidi '},{ name: 'Ezeh' },{ name: 'Ifeanyi'}]}/>
+      </div>
+    )
+  }
 }
 
 
