66a80aa910ae1b8cbf89201c77ae2633e0f87e5d
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-12-24 14:18:30 -0600

    Finishing The Contact Form - 1
    In this commit, we just build out the UI for the form with the css style we want. We have not make React control the state of this form just yet neither have we validated the input. One thing to note here is the ImageInput Component
    
    The ImageInput component is a custom <input> that dynamically reads and resizes image files before submitting them to the server as data URLs. It also shows a preview of the image. We chose to give this component to you rather than build it ourselves because it contains features related to files and images on the web that aren't crucial to your education in this context. If you're curious, feel free to dive into the code, but know it's not a requirement.

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom' //we imported React router Link Component beacuse we want to create a back button. Normal backButton form browser will still work though ;)
+import ImageInput from './ImageInput'
 
 class CreateContact extends Component {
   render() {
     return (
-      <div>Create Contact</div>
+      <div>
+        <Link
+          className='close-create-contact'
+          to='/'>
+            Close
+        </Link>
+        <form className='create-contact-form'>
+          <ImageInput
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+          />
+          <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='Name' />
+            <input type='text' name='handle' placeholder='Handle' />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
