e72bb4b4e27cee3e125b13154e660659b6253777
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-12-24 14:35:28 -0600

    Serialize The Form Data

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom' //we imported React router Link Component beacuse we want to create a back button. Normal backButton form browser will still work though ;)
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize' //instead of having the browser to serialize our form and throw the values into the url like '' we want to do that ourselves and save it into an object. The library will helps us acheive that
 
 class CreateContact extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault() //this will prevent the browser from serializing our form and  throwing the values of the form in the url
+    const values = serializeForm(e.target, { hash: true })
+    //console.log('values', values) //when you look at the log you see,  an object that represents our form data
+
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values) //we invoke this function that will be passed as a prop to CreateContact component that will not decide what to do with the data from our form
+    }
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +23,7 @@ class CreateContact extends Component {
           to='/'>
             Close
         </Link>
-        <form className='create-contact-form'>
+        <form className='create-contact-form' onSubmit={this.handleSubmit}>
           <ImageInput
             className='create-contact-avatar-input'
             name='avatarURL'
